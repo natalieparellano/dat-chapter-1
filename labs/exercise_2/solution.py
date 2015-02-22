722c4dafa120e4d8e3b6e8d1766edcc35b0b9edb
natalieparellano <natalie.p.arellano@gmail.com>
2015-02-22 12:36:48 -0500

    Add print statements

--- labs/exercise_2/solution.py
+++ labs/exercise_2/solution.py
@@ -1,10 +1,20 @@
 def find_sum():
     a, b     = 1, 1
     next_num = 2
+    print 'a is', a
+    print 'b is', b
+    print 'next is', next_num
     sum_even = 0
     while next_num < 4000000:
         if next_num % 2 == 0:
+            print '>>>>> adding', next_num
             sum_even += next_num
         a, b     = b, next_num
         next_num = a + b
+        print 'a is', a
+        print 'b is', b
+        print 'next is', next_num
     return sum_even  
+
+
+find_sum()
